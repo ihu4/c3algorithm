b588beeac1109dac9a57cbb9ae9b43352afd482e
lovew <lovew@DESKTOP-213LNAC>
2024-12-07 12:06:03 +0900

    초기 커밋.

---
+++ ads/ads.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <queue>
+using namespace std;
+int w[10];
+int main() {
+	stack<int> a;
+	queue<int> b;
+	int f;
+	for (int i = 0;i < 10;i++) {
+		cin >> f;
+		a.push(f);
+		b.push(f);
+	}
+	while (!a.empty()) {
+		cout << a.top() << " ";
+		a.pop();
+	}
+	cout << "\n";
+	while (!b.empty()) {
+		cout << b.front() << " ";
+		b.pop();
+	}
+	
+}
